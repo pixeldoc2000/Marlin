2afa8ed488f86827e2037f63bdb5438edc95f4d8
Luke Harrison <looxonline@gmail.com>
2020-11-15 13:18:28 +0200

    Fixed z_min

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -72,7 +72,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(thisiskeithb, BIQU B1 BLTouch)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(thisiskeithb/looxonline, BIQU B1 BLTouch)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -1124,7 +1124,7 @@
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
-#define Z_MIN_POS -5
+#define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE + 5
 #define Z_MAX_POS 270
