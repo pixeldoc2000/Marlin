58cef04b952cc645d03f9fd8e2b029969ef83e0d
Luke Harrison <looxonline@gmail.com>
2021-02-10 22:29:28 +0200

    Updates for sensorless homing

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2230,7 +2230,7 @@
 
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       1250        // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT_HOME  ((X_CURRENT)/2)  // (mA) RMS current for sensorless homing
+    #define X_CURRENT_HOME  450  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16    // 0..256
     #define X_RSENSE          0.15
     #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
@@ -2246,7 +2246,7 @@
 
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT       1250
-    #define Y_CURRENT_HOME  ((Y_CURRENT)/2)
+    #define Y_CURRENT_HOME  450
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.15
     #define Y_CHAIN_POS      -1
