c4f36ee6cb24e250c9b23203d82f036410b93b6d
Luke Harrison <looxonline@gmail.com>
2021-03-01 14:18:20 +0200

    Updates to UBL to slow the probing xy travel and disable HS probing which fails on some clones.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -999,7 +999,7 @@
 #define PROBING_MARGIN 4
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_SPEED (250*60)
+#define XY_PROBE_SPEED (150*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST (4*60)
